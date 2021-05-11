fd1eb3c6d482633bddde890de920c52c75b78878
PerMees <dppermees@gmail.com>
2021-05-11 21:07:33 +0700

    almost done

--- js/index.js
+++ js/index.js
@@ -186,13 +186,13 @@ console.log("Bài 2.1: " + numbers.toString());
 var user = document.getElementById("user");
 var btn = document.getElementById("user__btn");
 
-function alertUser() {
+function alertUserHandle() {
   if (user.value === "")
     alert("Mục người dùng không được bỏ trống, vui lòng nhập lại");
   else alert("Xin chào " + user.value.toLowerCase());
 }
 
-btn.onclick = alertUser;
+btn.onclick = alertUserHandle;
 
 /** Bài 2.3
  * Input: 3 số nguyên
@@ -222,7 +222,12 @@ function countEvenNumbers(numbers) {
 
 var result = countEvenNumbers(numbers);
 console.log(
-  "Có " + result + " số chẵn và " + (numbers.length - result) + " số lẻ"
+  "Bài 2.3: " +
+    "Có " +
+    result +
+    " số chẵn và " +
+    (numbers.length - result) +
+    " số lẻ"
 );
 
 /** Bài 2.4
@@ -250,7 +255,7 @@ var side2 = document.getElementById("rectangleSide2");
 var side3 = document.getElementById("rectangleSide3");
 var recBtn = document.getElementById("recBtn");
 
-function typeRec() {
+function alertTypeRecHandle() {
   let _side1, _side2, _side3;
   _side1 = Number(side1.value);
   _side2 = Number(side2.value);
@@ -296,4 +301,423 @@ function typeRec() {
   }
 }
 
-recBtn.onclick = typeRec;
+recBtn.onclick = alertTypeRecHandle;
+
+/** Bài 2.1*
+ * Input:  Ngày, tháng, năm
+ * Process:
+ *   B1: Tạo 3 biến _day, _month, _year để nhận 3 giá trị nhập từ bàn phím
+ *   B2: dùng switch case để xét các trường họp
+ *        - tháng 1,3,5,7,8,10
+ *        - tháng 12
+ *        - tháng 4,6,9,11
+ *        - tháng 2;
+ *   B3: trong mỗi case xét tiếp  _day có phải là
+ *       ngày cuối cùng của tháng hay không rồi tính toán
+ *   B4: In ra giá trị sau khi tính toán
+ * Output: ngày kế tiếp của ngày đã nhập
+ */
+
+var date = document.getElementById("date");
+var dateBtn = document.getElementById("dateBtn");
+
+function alertNextDateHandle() {
+  let _date = date.value.toString();
+  let _day = Number(_date.substr(_date.lastIndexOf("-") + 1));
+  _date = _date.substr(0, _date.lastIndexOf("-"));
+  let _month = Number(_date.substr(_date.lastIndexOf("-") + 1));
+  _date = _date.substr(0, _date.lastIndexOf("-"));
+  let _year = Number(_date);
+
+  switch (_month) {
+    case 1:
+    case 3:
+    case 5:
+    case 7:
+    case 8:
+    case 10:
+      if (_day === 31) {
+        _day = 1;
+        _month++;
+      } else _day++;
+      break;
+    case 12:
+      if (_day === 31) {
+        _day = 1;
+        _month = 1;
+        _year++;
+      } else _day++;
+      break;
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+      if (_day === 30) {
+        _day = 1;
+        _month++;
+      } else _day++;
+      break;
+    case 2:
+      if (_year % 100 === 0 && _year % 400 !== 0) {
+        if (_day === 28) {
+          _day = 1;
+          _month++;
+        } else _day++;
+      } else if (_year % 4 === 0) {
+        if (_day === 29) {
+          _day = 1;
+          _month++;
+        } else _day++;
+      } else {
+        if (_day === 28) {
+          _day = 1;
+          _month++;
+        } else _day++;
+      }
+  }
+  alert(_day + "/" + _month + "/" + _year);
+}
+
+dateBtn.onclick = alertNextDateHandle;
+
+/** Bài 2.2
+ * Input: tháng, năm nhập từ bàn phím
+ * Process:
+ *   B1: Tạo 3 biến _day, _month, _year để nhận 3 giá trị nhập từ bàn phím
+ *   B2: dùng switch case để xét các trường họp
+ *          - Tháng 1,3,5,7,8,10,12
+ *          - Tháng 2
+ *          - Tháng 4,6,9,11
+ *   B3: In ra số ngày trong tháng
+ * Output: ngày trong tháng
+ */
+
+var monthYear = document.getElementById("monthYear");
+var monthYearBtn = document.getElementById("monthYearBtn");
+
+function countDays() {
+  let _monthYear = monthYear.value.toString();
+  let _month = Number(_monthYear.substr(_monthYear.lastIndexOf("-") + 1));
+  _monthYear = _monthYear.substr(0, _monthYear.lastIndexOf("-"));
+  let _year = Number(_monthYear);
+  switch (_month) {
+    case 1:
+    case 3:
+    case 5:
+    case 7:
+    case 8:
+    case 10:
+    case 12:
+      alert("31");
+      break;
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+      alert("30");
+      break;
+    case 2:
+      if (_year % 100 === 0 && _year % 400 !== 0) {
+        alert("28");
+      } else if (_year % 4 === 0) {
+        alert("29");
+      } else {
+        alert("28");
+      }
+      break;
+  }
+}
+
+monthYearBtn.onclick = countDays;
+
+/** Bài 2.3
+ *
+ */
+
+var readNum = document.getElementById("readNum");
+var readNumBtn = document.getElementById("readNumBtn");
+
+function readNumHandle() {
+  let num = Number(readNum.value);
+  let result = "";
+  switch (num % 10) {
+    case 1:
+      result = "một" + result;
+      break;
+    case 2:
+      result = "hai" + result;
+      break;
+    case 3:
+      result = "ba" + result;
+      break;
+    case 4:
+      result = "bốn" + result;
+      break;
+    case 5:
+      result = "năm" + result;
+      break;
+    case 6:
+      result = "sáu" + result;
+      break;
+    case 7:
+      result = "bảy" + result;
+      break;
+    case 8:
+      result = "tám" + result;
+      break;
+    case 9:
+      result = "chín" + result;
+      break;
+  }
+  num = Math.floor(num / 10);
+  switch (num % 10) {
+    case 0:
+      if (Number(readNum.value) % 100 !== 0) result = "lẻ " + result;
+      break;
+    case 1:
+      result = "mười " + result;
+      break;
+    case 2:
+      result = "hai mươi " + result;
+      break;
+    case 3:
+      result = "ba mươi " + result;
+      break;
+    case 4:
+      result = "bốn mươi " + result;
+      break;
+    case 5:
+      result = "năm mươi " + result;
+      break;
+    case 6:
+      result = "sáu mươi " + result;
+      break;
+    case 7:
+      result = "bảy mươi " + result;
+      break;
+    case 8:
+      result = "tám mươi " + result;
+      break;
+    case 9:
+      result = "chín mươi " + result;
+      break;
+  }
+  num = Math.floor(num / 10);
+  switch (num) {
+    case 0:
+      if (Number(readNum.value) % 100 !== 0) result = "lẻ " + result;
+      break;
+    case 1:
+      result = "Một trăm " + result;
+      break;
+    case 2:
+      result = "Hai trăm " + result;
+      break;
+    case 3:
+      result = "Ba trăm " + result;
+      break;
+    case 4:
+      result = "Bốn trăm " + result;
+      break;
+    case 5:
+      result = "Năm trăm " + result;
+      break;
+    case 6:
+      result = "Sáu trăm " + result;
+      break;
+    case 7:
+      result = "Bảy trăm " + result;
+      break;
+    case 8:
+      result = "Tám trăm " + result;
+      break;
+    case 9:
+      result = "Chín trăm " + result;
+      break;
+  }
+  alert(result);
+}
+
+readNumBtn.onclick = readNumHandle;
+
+/** Bài 2.4
+ *  Input: Tọa độ trường và 3 học sinh
+ *  Process:
+ *    B1: Tạo các biến để nhận các giá trị tọa độ
+ *    B2: Tạo biến maxDistance và maxDistanceStu để lưu trữ học sinh có nhà cách xa nhất và khoảng cách
+ *    B3: Mặc định gán cho học sinh 1 nhà xa nhất, lần lượt so sánh với hs 2 và 3.
+ *    B4: In ra học sinh có khoảng cách từ nhà đến trường xa nhất
+ *  Output: Học sinh có nhà cách trường xa nhất
+ */
+
+var distanceBtn = document.getElementById("distanceBtn");
+var schoolX = document.getElementById("schoolX");
+var schoolY = document.getElementById("schoolY");
+var st1X = document.getElementById("st1X");
+var st1Y = document.getElementById("st1Y");
+var st2X = document.getElementById("st2X");
+var st2Y = document.getElementById("st2Y");
+var st3X = document.getElementById("st3X");
+var st3Y = document.getElementById("st3Y");
+
+function calcDistance() {
+  let _schoolX = Number(schoolX.value);
+  let _schoolY = Number(schoolY.value);
+  let _st1X = Number(st1X.value);
+  let _st1Y = Number(st1Y.value);
+  let _st2X = Number(st2X.value);
+  let _st2Y = Number(st2Y.value);
+  let _st3X = Number(st3X.value);
+  let _st3Y = Number(st3Y.value);
+  let maxDistance = Math.sqrt(
+    (_st1X - _schoolX) ** 2 + (_st1Y - _schoolY) ** 2
+  );
+  let maxDistanceStu = 1;
+  if (
+    maxDistance < Math.sqrt((_st2X - _schoolX) ** 2 + (_st2Y - _schoolY) ** 2)
+  ) {
+    maxDistance = Math.sqrt((_st2X - _schoolX) ** 2 + (_st2Y - _schoolY) ** 2);
+    maxDistanceStu = 2;
+  }
+  if (
+    maxDistance < Math.sqrt((_st3X - _schoolX) ** 2 + (_st3Y - _schoolY) ** 2)
+  ) {
+    maxDistance = Math.sqrt((_st3X - _schoolX) ** 2 + (_st3Y - _schoolY) ** 2);
+    maxDistanceStu = 3;
+  }
+  alert("Học sinh " + maxDistanceStu + " nhà cách trường xa nhất");
+}
+
+distanceBtn.onclick = calcDistance;
+
+// Buổi 3
+
+/** Bài 3.1: Quản lý tuyển sinh
+ * Input: Điểm chuẩn, điểm 3 môn, khu vực, đối tượng dự thi
+ * Process:
+ *    B1: Tạo các biến để gán giá trị điểm chuẩn, điểm 3 môn, khu vực,
+ *      đối tượng, tổng điểm, kết quả
+ *    B2: Tính tổng điểm
+ *    B3: So sánh với điểm chuẩn
+ *    B4: In ra tổng điểm và kết quả
+ * Output: kết quả, tổng điểm
+ */
+
+var checkMarkBtn = document.getElementById("checkMarkBtn");
+var baseMark = document.getElementById("baseMark");
+var mark1 = document.getElementById("mark1");
+var mark2 = document.getElementById("mark2");
+var mark3 = document.getElementById("mark3");
+var studentArea = document.getElementById("studentArea");
+var studentObject = document.getElementById("studentObject");
+var markAndResult = document.getElementById("markAndResult");
+var result = document.getElementById("result");
+var totalMark = document.getElementById("totalMark");
+
+function checkMark() {
+  let _baseMark = Number(baseMark.value);
+  let _mark1 = Number(mark1.value);
+  let _mark2 = Number(mark2.value);
+  let _mark3 = Number(mark3.value);
+  let _studentArea = studentArea.value;
+  let _studentObject = studentObject.value;
+  let _totalMark = 0;
+
+  _totalMark = _mark1 + _mark2 + _mark3;
+
+  if (_studentArea === "A") _totalMark += 2;
+  else if (_studentArea === "B") _totalMark += 1;
+  else if (_studentArea === "C") _totalMark += 0.5;
+
+  if (_studentObject === "1") _totalMark += 2.5;
+  else if (_studentObject === "2") _totalMark += 1.5;
+  else if (_studentObject === "3") _totalMark += 1;
+
+  totalMark.innerHTML = _totalMark;
+
+  if (_totalMark < _baseMark) result.innerHTML = "Rớt";
+  else result.innerHTML = "Đậu";
+
+  markAndResult.style.display = "initial";
+}
+
+checkMarkBtn.onclick = checkMark;
+
+/** Bài 3.2: Tính tiền điện
+ * Input: Tên, Số điện
+ * Process:
+ *    B1:Tạo biến để nhập số điện, tạo biến tổng tiền
+ *    B2: Tính toán số tiền điện
+ *     if >350; if >200; if >100; if >50;
+ *    B3: in ra kết quả
+ * Output: Số tiền điện
+ */
+
+var elecUserName = document.getElementById("elecUserName");
+var elecNumber = document.getElementById("elecNumber");
+var totalElecMoney = document.getElementById("totalElecMoney");
+var elec__name = document.getElementById("elec__name");
+var calcElecBtn = document.getElementById("calcElecBtn");
+var reslutCalcElecMoney = document.getElementById("reslutCalcElecMoney");
+function calcElecMoney() {
+  var _totalElecMoney = 0;
+  var _elecNumber = Number(elecNumber.value);
+  if (_elecNumber > 350) {
+    _totalElecMoney += 1300 * (_elecNumber - 350);
+    _elecNumber = 350;
+  }
+  if (_elecNumber > 200) {
+    _totalElecMoney += 1100 * (_elecNumber - 200);
+    _elecNumber = 200;
+  }
+  if (_elecNumber > 100) {
+    _totalElecMoney += 850 * (_elecNumber - 100);
+    _elecNumber = 100;
+  }
+  if (_elecNumber > 50) {
+    _totalElecMoney += 650 * (_elecNumber - 50);
+    _elecNumber = 50;
+  }
+  _totalElecMoney += 500 * _elecNumber;
+
+  elec__name.innerHTML = elecUserName.value;
+  elec__name.style.textTransform = "capitalize";
+  totalElecMoney.innerHTML = _totalElecMoney;
+  reslutCalcElecMoney.style.display = "initial";
+}
+
+calcElecBtn.onclick = calcElecMoney;
+
+/** Bài 3.3: Tính thuế thu nhập cá nhân
+ * Input: Họ tên, Tổng thu nhập năm, số người phụ thuộc
+ * Output: Thuế thu nhập cá nhân
+ */
+
+var tax__name = document.getElementById("tax__name");
+var calcTaxBtn = document.getElementById("calcTaxBtn");
+var salaryPerYear = document.getElementById("salaryPerYear");
+var peopleDepend = document.getElementById("peopleDepend");
+var reslutCalcTax = document.getElementById("reslutCalcTax");
+var totalTax = document.getElementById("totalTax");
+
+function calcTax() {
+  let _salaryPerYear = Number(salaryPerYear.value);
+  let _peopleDepend = Number(peopleDepend.value);
+  let _totalTax = 1;
+  _totalTax *= _salaryPerYear - 4 - 1.6 * _peopleDepend;
+  if (_salaryPerYear > 960) _totalTax *= 0.35;
+  else if (_salaryPerYear > 624) _totalTax *= 0.3;
+  else if (_salaryPerYear > 384) _totalTax *= 0.25;
+  else if (_salaryPerYear > 210) _totalTax *= 0.2;
+  else if (_salaryPerYear > 120) _totalTax *= 0.15;
+  else if (_salaryPerYear > 60) _totalTax *= 0.1;
+  else _totalTax *= 0.05;
+
+  tax__name.innerHTML = document.getElementById("taxName").value;
+  tax__name.style.textTransform = "capitalize";
+  totalTax.innerHTML = _totalTax.toFixed(2) + "  triệu";
+  reslutCalcTax.style.display = "initial";
+}
+
+calcTaxBtn.onclick = calcTax;
+
